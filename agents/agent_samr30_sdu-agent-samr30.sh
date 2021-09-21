0a3aa022f05e8ac19fad5ae6859328a07e436208
L. Koetting <59482387+Koettinl@users.noreply.github.com>
2021-09-21 15:03:03 +0200

    Update agent_samr30_sdu-agent-samr30.sh

--- agents/agent_samr30_sdu-agent-samr30.sh
+++ agents/agent_samr30_sdu-agent-samr30.sh
@@ -47,11 +47,10 @@ install_update () {
 	local UpdateFile=$(tar -xvf $FILENAME-$VERSION)
 
 	# flash *.bin to samr30 via edbg
-	# ~/path/to/edbg -t $BOARD -p -f ~/path/to/*.bin to be installed or flashed
-	# /home/pi/ instead of ~ for systemd to be able to find path
-	# in this case an example Hello World is used
+	# /home/pi/path/to/edbg -t $BOARD -p -f /home/pi/path/to/*.bin to be installed or flashed
 	/home/pi/bin/edbg -t samr30 -pv -f /home/pi/$UpdateFile	
-	# end
+	
+	# write logfile
 	echo -e "$(date -u) samr updated to $VERSION\n" >>/home/pi/sdu_guc_ssv/clients/sam-r30/sam-r30_fwUpdate_logfile.txt
 }
 
