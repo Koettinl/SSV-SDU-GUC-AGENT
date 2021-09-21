925e9bccef313f23e7a2178e99edabd673a7b6f8
L. Koetting <59482387+Koettinl@users.noreply.github.com>
2021-09-21 14:59:33 +0200

    Add files via upload
    Ordnerstruktur erzeugt

--- old/update.sh
+++ old/update.sh
@@ -4,8 +4,8 @@ Filepath=/home/pi/sdu_guc_ssv
 
 # call updateclient for dummy agent
 #	[path to GUC | path to used agent]
-$Filepath/clients/guc_sdu-update.sh $Filepath/agents/agent_samr30_sdu-agent-samr30.sh
+$Filepath/agents/guc_sdu-update.sh $Filepath/agents/agent_dummy_sdu-agent-dummy.sh
 
 # call read_Sam_R30.sh to create a logfile
 #	~/gpio_testing
-$Filepath/products/sam-r30/read_Sam_R30.sh
+$Filepath/clients/sam-r30/read_Sam_R30.sh
