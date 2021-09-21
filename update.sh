f60c1fe16ee019e9eec918f7c6e7b2e9afbcbdee
L. Koetting <59482387+Koettinl@users.noreply.github.com>
2021-09-21 17:20:48 +0200

    Update update.sh
    read-samr30 entfernt

--- update.sh
+++ update.sh
@@ -5,7 +5,3 @@ Filepath=/home/pi/sdu_guc_ssv
 # call updateclient for dummy agent
 #	[path to GUC | path to used agent]
 $Filepath/clients/guc_sdu-update.sh $Filepath/agents/agent_samr30_sdu-agent-samr30.sh
-
-# call read_Sam_R30.sh to create a logfile
-#	~/gpio_testing
-$Filepath/products/sam-r30/read_Sam_R30.sh
